0e49473cc9a27e2577dd6eec6e8c47d2473ec2ca
Jeet Kumar Ramnani <jeetkramnani@gmail.com>
2023-07-15 01:50:15 +0500

    Added Functionality Like Dark Mode, Success Toast

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const stdCollegeName = d.querySelector("#stdCollegeName");
 const stdStudyField = d.querySelector("#stdStudyField");
 const stdPassword = d.querySelector("#stdPassword");
 const submitBtn = d.querySelector("#submitButton");
+const themeBtn = d.querySelector("#themeBtn");
 console.log(submitBtn);
 
 const studentData = {
@@ -15,6 +16,17 @@ const studentData = {
   stdPassword,
 };
 
+themeBtn.addEventListener("click", () => {
+  const darkMode = document.body.classList.toggle("bg-black");
+  // document.body.classList.add("transition")
+  themeBtn.classList.toggle("bg-white");
+  themeBtn.classList.toggle("text-white");
+
+  themeBtn.textContent = darkMode
+    ? "Switch To Light Mode"
+    : "Switch To Dark Mode";
+});
+
 submitBtn.addEventListener("click", function (e) {
   e.preventDefault();
   console.log(e);
@@ -24,5 +36,23 @@ submitBtn.addEventListener("click", function (e) {
   studentData.stdStudyField = stdStudyField.value;
   studentData.stdPassword = stdPassword.value;
   localStorage.setItem("studentsObject", JSON.stringify(studentData));
-  location.href = "./GetYourData.html";
+  const notifierToast = document.createElement("div");
+  notifierToast.innerHTML = "Data Saved Successfully";
+  notifierToast.classList.add(
+    "bg-green-400",
+    "p-3",
+    "font-semibold",
+    "font-sans",
+    "inline-block",
+    "absolute",
+    "top-1",
+    "left-1"
+  );
+  document.body.appendChild(notifierToast);
+  setTimeout(() => {
+    notifierToast.style.display = "none";
+    setTimeout(() => {
+      location.href = "./GetYourData.html";
+    }, 2000);
+  }, 1000);
 });
